404054355812165147c75175042dfdd0270658ff
Finn <me@finn.lu>
2015-05-03 02:06:32 +0200

    Husky

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+
+namespace EloBuddy.TestRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.OnStart += a =>
+            {
+                Game.PrintChat("Thank you spudgy for developing our loader!");
+            };
+        }
+    }
+}
